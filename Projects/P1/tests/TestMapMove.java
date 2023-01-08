b45e9b520e277227d40e225f9ecde2a4cb7e8d40
Nate Perry <58754480+nate-perry@users.noreply.github.com>
2023-01-08 13:30:41 -0500

    added test for map.move()

--- Projects/P1/tests/TestMapMove.java
+++ Projects/P1/tests/TestMapMove.java
@@ -1,9 +1,21 @@
 import java.io.*;
 import junit.framework.*;
+import javax.swing.JComponent;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class TestMapMove extends TestCase {
 
   public void testMapMove() throws FileNotFoundException {
-    return null;
+    Map map = new Map(10);
+    Location location1 = new Location(2, 2);
+    Location location2 = new Location(3, 3);
+    JComponent component = new JComponent() {
+
+    };
+
+    map.add("pacman1", location1, component, Map.Type.PACMAN);
+
+    assertTrue(map.move("pacman1", location2, Map.Type.PACMAN));
   }
 }
