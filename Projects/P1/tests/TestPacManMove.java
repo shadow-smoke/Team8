f8c57486904e2e5499c8ebd6d962d2f85d8edf34
Fernando Gonzales-Vigil <fgovir@gmail.com>
2023-01-08 23:11:06 -0500

    Comments added to test for clarity

--- Projects/P1/tests/TestPacManMove.java
+++ Projects/P1/tests/TestPacManMove.java
@@ -10,7 +10,7 @@ public class TestPacManMove extends TestCase {
   * the chose location is one of the possible moves.
    */
   public void testPacManMove() throws FileNotFoundException {
-    // new Location(9, 11);
+    // creating dummy data representing get_valid_moves result
     Location oldLocation = new Location(5, 10);
     Map mp = new Map(20);
     PacMan pacman = new PacMan("pacman", oldLocation, mp);
@@ -25,11 +25,11 @@ public class TestPacManMove extends TestCase {
     possibleMoves.add(new Location(6, 11));
     int numMoves = possibleMoves.size();
 
-    // reimplement moves in Test to verify algorithm
+    // reimplementing moves in Test to verify algorithm
     Random rn = new Random();
-
     int rd_loc_index = rn.nextInt(numMoves);
     pacman.myLoc = possibleMoves.get(rd_loc_index);
+    // updating location in map
     pacman.myMap.move(pacman.myName, pacman.myLoc, Map.Type.PACMAN);
 
     assertTrue(possibleMoves.contains(pacman.myLoc));
