06be679f08e3082207e5444496b9276517fad4da
Fernando Gonzales-Vigil <fernandogonzales-vigil@osgconnect.com>
2023-01-11 16:16:40 -0500

    Fix Test errors

--- Projects/P1/tests/TestAttack.java
+++ Projects/P1/tests/TestAttack.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.io.*;
 import junit.framework.*;
 
@@ -9,7 +10,7 @@ public class TestAttack extends TestCase {
     NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
 
     //Creating Players
-    Ghost ghost = frame.addGhost(new Location(9, 11), "Boo", frame); //Creates a red ghost named "name" at location x,y
+    Ghost ghost = frame.addGhost(new Location(9, 11), "Boo", Color.red); //Creates a red ghost named "name" at location x,y
     PacMan pacman = frame.addPacMan(new Location(9, 12)); //Creates PacMan at location x, y
 
     boolean attackSuccessfull = ghost.attack();
@@ -24,7 +25,7 @@ public class TestAttack extends TestCase {
     NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
 
     //Creating Players
-    Ghost ghost = frame.addGhost(new Location(9, 11), "Boo", frame); //Creates a red ghost named "name" at location x,y
+    Ghost ghost = frame.addGhost(new Location(9, 11), "Boo", Color.red); //Creates a red ghost named "name" at location x,y
     PacMan pacman = frame.addPacMan(new Location(9, 15)); //Creates PacMan at location x, y
 
     boolean attackSuccessfull = ghost.attack();
--- Projects/P1/tests/TestPacManInRange.java
+++ Projects/P1/tests/TestPacManInRange.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.io.*;
 import junit.framework.*;
 
