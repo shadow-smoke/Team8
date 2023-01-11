c7e7b15f7d07528e139b7fcd4e191b6f74feb372
sukhmank11 <sukhmank11@gmail.com>
2023-01-11 17:58:13 -0500

    changed eatcookie test

--- Projects/P1/tests/TestMapEatCookie.java
+++ Projects/P1/tests/TestMapEatCookie.java
@@ -4,20 +4,25 @@ import junit.framework.*;
 public class TestMapEatCookie extends TestCase {
 
   public void testMapEatCookie() throws FileNotFoundException {
-    Location locate = new Location(9, 12);
-    Map area = new Map(20);
-    PacMan pacman = new PacMan("pacman", locate, area);
-    int row = 9;
-    int col = 11;
-    Location cookieLoc = new Location(9, 11);
-    CookieComponent cookie = new CookieComponent(9, 11, 20);
-    String id = "tok_x9_y11";
+    // Location locate = new Location(9, 12);
+    // Map area = new Map(20);
+    // PacMan pacman = new PacMan("pacman", locate, area);
+    // int row = 9;
+    // int col = 11;
+    // Location cookieLoc = new Location(9, 11);
+    // CookieComponent cookie = new CookieComponent(9, 11, 20);
+    // String id = "tok_x9_y11";
 
-    area.add(id, cookieLoc, cookie, Map.Type.COOKIE);
+    // area.add(id, cookieLoc, cookie, Map.Type.COOKIE);
  
-    cookie.setLocation(9, 11);
+    // cookie.setLocation(9, 11);
+
+    NoFrame frame = new NoFrame();
+    Map map = frame.getMap();
+    PacMan pac = frame.addPacMan(new Location(1,1));
+
     
-    assertTrue(area.eatCookie(id) != null);
-    assertTrue(area.eatCookie(id) == null);
+    assertTrue(map.eatCookie(pac.myName) != null);
+    assertTrue(map.eatCookie(pac.myName) == null);
   }
 }
