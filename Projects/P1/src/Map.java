e630b196e77e2cac6975ad5f42ff6ab21455f6d0
sukhmank11 <sukhmank11@gmail.com>
2023-01-10 19:14:08 -0500

    added eatCookie and test

--- Projects/P1/src/Map.java
+++ Projects/P1/src/Map.java
@@ -70,6 +70,17 @@ public class Map {
   public JComponent eatCookie(String name) {
     // update locations, components, field, and cookies
     // the id for a cookie at (10, 1) is tok_x10_y1
-    return null;
+    if(!components.containsKey(name)){
+      return null;
+    }
+    cookies++;
+    Location cookieLoc = locations.get(name);
+    JComponent cookieEaten = components.get(name);
+    components.remove(name);
+    locations.replace(name, null);
+    field.remove(cookieLoc);
+
+
+    return cookieEaten;
   }
 }
