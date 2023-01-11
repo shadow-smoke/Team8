fe00e88ad8672fed1ce1d7b3a5b2a03f579c1b7a
sukhmank11 <sukhmank11@gmail.com>
2023-01-11 17:53:41 -0500

    consume test

--- Projects/P1/tests/TestConsume.java
+++ Projects/P1/tests/TestConsume.java
@@ -8,18 +8,21 @@ public class TestConsume extends TestCase {
 
   public void testConsume() throws FileNotFoundException {
     
-    Location locate = new Location(9, 12);
-    Map area = new Map(20);
-    PacMan pacman = new PacMan("pacman", locate, area);
-    int row = 9;
-    int col = 12;
-    Location cookieLoc = new Location(9, 12);
-    CookieComponent cookie = new CookieComponent(9, 12, 20);
+    // Location locate = new Location(9, 12);
+    // Map area = new Map(20);
+    // PacMan pacman = new PacMan("pacman", locate, area);
+    // int row = 9;
+    // int col = 12;
+    // Location cookieLoc = new Location(9, 12);
+    // CookieComponent cookie = new CookieComponent(9, 12, 20);
 
-    area.add("tok_x" + row + "_y" + col, cookieLoc, cookie, Map.Type.COOKIE);
+    // area.add("tok_x" + row + "_y" + col, cookieLoc, cookie, Map.Type.COOKIE);
 
-    
-    assertTrue(pacman.consume() != null);
+    //System.out.println(pacman.consume());
+    NoFrame frame = new NoFrame();
+    Map map = frame.getMap();
+    PacMan pac = frame.addPacMan(new Location(1,1));
+    assertTrue(pac.consume() != null);
 
 
   }
